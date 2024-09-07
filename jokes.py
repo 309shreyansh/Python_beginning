624c0ad6bf0423774107c3afdcea43415cafb45c
Shreyansh Singh <sshreyansh336@gmail.com>
2024-09-07 23:00:24 +0530

    about the single line and multi line comment

--- jokes.py
+++ jokes.py
@@ -1,4 +1,17 @@
 import pyjokes
 
+# print("printing jokes.....")
+# this prints a random jokes 
+# this sign is made with ctrl+/ and used for single line comment
+
+
+
 joke=pyjokes.get_jokes()
 print(joke)
+
+"""
+so thanks 
+that was 
+my code (the double quote is used for multi line comments)
+
+"""
