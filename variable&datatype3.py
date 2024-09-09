eec3df8e52ad03d89a207f7ff89ebc3fc2c261f5
Shreyansh Singh <sshreyansh336@gmail.com>
2024-09-09 23:59:25 +0530

    all about operators

---
+++ variable&datatype3.py
@@ -0,0 +1,38 @@
+#Arithmetic operator
+
+a=7
+b=4
+c=a+b
+print(c)
+
+#Assignment operator
+
+a=4-2 #Assign 4-2 in a 
+print(a)
+
+b=6
+b+=3 #increment the value of b by 3 and then assign it to b 
+print(b)
+
+#comparison operators
+d=5>4
+print(d)
+
+#Logical operators
+
+e= True or False
+print(e)
+
+#Truth table of "or"
+print("True or False is",True or False)
+print("True or True is",True or True)
+print("False or True is", False or True)
+print("False or False is",False or False)
+
+#Truth table of "and"
+print("True and False is ", True and False)
+print("True and True ", True and True )
+print("False and true is ",False and true)
+print("False and False", False and False)
+
+print(not(False))
