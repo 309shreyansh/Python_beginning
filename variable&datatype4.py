7c2d6470dad60437a86e823fb7d3cbecd8eaf2b1
Shreyansh Singh <sshreyansh336@gmail.com>
2024-09-10 18:01:00 +0530

    sting float int

---
+++ variable&datatype4.py
@@ -0,0 +1,15 @@
+#<class 'int'> u will get 
+a=2
+t=type(a) #class<int>
+print(t)
+
+#<class 'float'> u will get
+b=31.2
+t=type(b) #class<int>
+print(t)
+
+#<class 'str'> u will get
+#if a number is also written inside a string we will get <class 'str'>
+c="Shreyansh"
+t=type(c) #class<int
+print(t)
